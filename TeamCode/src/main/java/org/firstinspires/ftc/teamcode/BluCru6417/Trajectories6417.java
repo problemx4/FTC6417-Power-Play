30880fcced13222a6f299b43013c857a64a3e0d3
Andrew Smith <AndrewTabSmith@gmail.com>
2022-11-05 14:43:53 -0400

    Removed unused trajectories

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BluCru6417/Trajectories6417.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BluCru6417/Trajectories6417.java
@@ -63,131 +63,8 @@ public class Trajectories6417 {
                 })
                 .build();
     }
-    public static TrajectorySequence redAudienceSideBuilder (SampleMecanumDrive drive){
-        return drive.trajectorySequenceBuilder(new Pose2d(-36, -65, Math.toRadians(90)))
-                .setTangent(Math.toRadians(0))
-                .splineToConstantHeading(new Vector2d(-12, -53), Math.toRadians(90))
-                .splineToSplineHeading(new Pose2d(-12, -36,Math.toRadians(135)), Math.toRadians(90))
-                .setVelConstraint(SampleMecanumDrive.getVelocityConstraint(10, Math.toRadians(180), 12))
-                .UNSTABLE_addTemporalMarkerOffset(0,() -> {
-                    //raise slider and prepare it to place
-                })
-                .forward(6)
-                .UNSTABLE_addTemporalMarkerOffset(0, () -> {
-                    //drop cone
-                })
-                .resetVelConstraint()
-                .setTangent(Math.toRadians(-45))
-                .splineToLinearHeading(new Pose2d(-12, -36, Math.toRadians(180)), Math.toRadians(-45))
-                .UNSTABLE_addTemporalMarkerOffset(0,() -> {
-                    //drop slider
-                })
-                .setTangent(Math.toRadians(180))
-                .splineTo(new Vector2d(-24,-36), Math.toRadians(180))
-                .setVelConstraint(SampleMecanumDrive.getVelocityConstraint(10, Math.toRadians(180), 12.5))
-                .forward(6)
-                .resetVelConstraint()
-                .UNSTABLE_addTemporalMarkerOffset(.5,() -> {
-                    //grab cone after running into it for .5 sec
-                })
-                .splineToSplineHeading(new Pose2d(-36,-36, Math.toRadians(45)), Math.toRadians(180))
-                .setVelConstraint(SampleMecanumDrive.getVelocityConstraint(10, Math.toRadians(180), 12.5))
-                .UNSTABLE_addTemporalMarkerOffset(0,() -> {
-                    //raise slider and prepare it to place
-                })
-                .forward(6)
-                .UNSTABLE_addTemporalMarkerOffset(0,() -> {
-                    //drop cone
-                })
-                .resetVelConstraint()
-                .setTangent(Math.toRadians(-135))
-                .splineToSplineHeading(new Pose2d(-36,-36, Math.toRadians(90)), Math.toRadians(-135))
-                .build();
-    }
 
-    public static TrajectorySequence blueStageSideBuilder (SampleMecanumDrive drive){
-        return drive.trajectorySequenceBuilder(new Pose2d(36, 65, Math.toRadians(-90)))
-                .setTangent(Math.toRadians(180))
-                .splineToConstantHeading(new Vector2d(12, 53), Math.toRadians(-90))
-                .splineToSplineHeading(new Pose2d(12, 36,Math.toRadians(-45)), Math.toRadians(-90))
-                .setVelConstraint(SampleMecanumDrive.getVelocityConstraint(10, Math.toRadians(180), 12))
-                .UNSTABLE_addTemporalMarkerOffset(0,() -> {
-                    //raise slider and prepare it to place
-                })
-                .forward(6)
-                .UNSTABLE_addTemporalMarkerOffset(0, () -> {
-                    //drop cone
-                })
-                .resetVelConstraint()
-                .setTangent(Math.toRadians(-225))
-                .splineToLinearHeading(new Pose2d(12, 36, Math.toRadians(0)), Math.toRadians(-225))
-                .UNSTABLE_addTemporalMarkerOffset(0,() -> {
-                    //drop slider
-                })
-                .setTangent(Math.toRadians(0))
-                .splineTo(new Vector2d(24,36), Math.toRadians(0))
-                .setVelConstraint(SampleMecanumDrive.getVelocityConstraint(10, Math.toRadians(180), 12.5))
-                .forward(6)
-                .resetVelConstraint()
-                .UNSTABLE_addTemporalMarkerOffset(.5,() -> {
-                    //grab cone after running into it for .5 sec
-                })
-                .splineToSplineHeading(new Pose2d(36,36, Math.toRadians(-135)), Math.toRadians(0))
-                .setVelConstraint(SampleMecanumDrive.getVelocityConstraint(10, Math.toRadians(180), 12.5))
-                .UNSTABLE_addTemporalMarkerOffset(0,() -> {
-                    //raise slider and prepare it to place
-                })
-                .forward(6)
-                .UNSTABLE_addTemporalMarkerOffset(0,() -> {
-                    //drop cone
-                })
-                .resetVelConstraint()
-                .setTangent(Math.toRadians(45))
-                .splineToSplineHeading(new Pose2d(36,36, Math.toRadians(-90)), Math.toRadians(45))
-                .build();
-    }
 
-    public static TrajectorySequence blueAudienceSideBuilder (SampleMecanumDrive drive){
-        return drive.trajectorySequenceBuilder(new Pose2d(-36, 65, Math.toRadians(-90)))
-                .setTangent(Math.toRadians(0))
-                .splineToConstantHeading(new Vector2d(-12, 53), Math.toRadians(-90))
-                .splineToSplineHeading(new Pose2d(-12, 36,Math.toRadians(-135)), Math.toRadians(-90))
-                .setVelConstraint(SampleMecanumDrive.getVelocityConstraint(10, Math.toRadians(180), 12))
-                .UNSTABLE_addTemporalMarkerOffset(0,() -> {
-                    //raise slider and prepare it to place
-                })
-                .forward(6)
-                .UNSTABLE_addTemporalMarkerOffset(0, () -> {
-                    //drop cone
-                })
-                .resetVelConstraint()
-                .setTangent(Math.toRadians(45))
-                .splineToLinearHeading(new Pose2d(-12, 36, Math.toRadians(-180)), Math.toRadians(45))
-                .UNSTABLE_addTemporalMarkerOffset(0,() -> {
-                    //drop slider
-                })
-                .setTangent(Math.toRadians(-180))
-                .splineTo(new Vector2d(-24,36), Math.toRadians(-180))
-                .setVelConstraint(SampleMecanumDrive.getVelocityConstraint(10, Math.toRadians(180), 12.5))
-                .forward(6)
-                .resetVelConstraint()
-                .UNSTABLE_addTemporalMarkerOffset(.5,() -> {
-                    //grab cone after running into it for .5 sec
-                })
-                .splineToSplineHeading(new Pose2d(-36,36, Math.toRadians(-45)), Math.toRadians(-180))
-                .setVelConstraint(SampleMecanumDrive.getVelocityConstraint(10, Math.toRadians(180), 12.5))
-                .UNSTABLE_addTemporalMarkerOffset(0,() -> {
-                    //raise slider and prepare it to place
-                })
-                .forward(6)
-                .UNSTABLE_addTemporalMarkerOffset(0,() -> {
-                    //drop cone
-                })
-                .resetVelConstraint()
-                .setTangent(Math.toRadians(135))
-                .splineToSplineHeading(new Pose2d(-36,36, Math.toRadians(-90)), Math.toRadians(135))
-                .build();
-    }
 
     public static Trajectory firstParkPositionBuilder (SampleMecanumDrive drive){
         return drive.trajectoryBuilder(new Pose2d(36,-12, Math.toRadians(0)), Math.toRadians(0))
@@ -206,6 +83,8 @@ public class Trajectories6417 {
                 .forward(24)
                 .build();
     }
+
+
 
     public static boolean isCyclePossible(double elapsedTime){
         double cycleTime = 8;
