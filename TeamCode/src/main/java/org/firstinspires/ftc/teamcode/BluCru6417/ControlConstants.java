397785b09574fe29cb63cfb1fd4217abff0ee852
Andrew Smith <AndrewTabSmith@gmail.com>
2022-11-19 13:23:18 -0500

    Cleaned up control constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BluCru6417/ControlConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BluCru6417/ControlConstants.java
@@ -1,42 +1,30 @@
 package org.firstinspires.ftc.teamcode.BluCru6417;
 
 public interface ControlConstants {
-    /* measurement variables */
-    // calculate these in inches
-    double DIAMETER                 = 3.77952756;
-    double CPR                      = 800;
-    double CIRC                     = DIAMETER * Math.PI;
-
     //angle variables
-    int armHorizontalPosition       = 100; //MEASURE THIS
-
     double armServoVerticalPosition = .5; //MEASURE THIS
     double armServoHorizontalPosition = 1.0; //MEASURE THIS
 
     double wristVerticalPosition    = .5; //MEASURE THIS
     double wristHorizontalPosition  = 1.0;
 
-    //maintenance variable
-    double armMaintenanceCoeff      = .01; //MEASURE THIS
-
 
     /* control variables */
     //power variables
     double maxDrivePower            = 0.6;
     double maxSlowerDrivePower      = 0.3;
-    double maxSliderPower           = 0.4;
-    double maxArmPower              = 0.2;
 
-    double autoSlideSpeed           = 0.6;
-    double autoArmSpeed             = 0.5;
+    double maxSliderPower           = 0.4;
+    double returnSliderPower        = 0.15;
+    double autoSlidePower           = 0.6;
 
     double sens                     = 0.05;
     double triggerSens              = 0.5;
 
     double manualWristDelta         = 0.01;
 
-    double grabberClosePos          = 0.0;
-    double grabberOpenPos           = 0.12;
+    double grabberClosePos          = 1.0;
+    double grabberOpenPos           = 0.78;
 
     double armServoDownPos          = 0.5;
     double armServoForwardPos       = armServoHorizontalPosition;
@@ -45,13 +33,8 @@ public interface ControlConstants {
 
     //encoder tick variables
     int sliderBasePos               = 0;
-    int sliderLowPos                = 100;
-    int sliderMedPos                = 200;
-    int sliderHighPos               = 300;
-    int sliderMaxPos                = 400;
-
-    int armDownPos                  = 0;
-    int armForwardPos               = armHorizontalPosition;
-    int armBackwardsPos             = -armHorizontalPosition;
-    int armUsefulPos                = 100;
+    int sliderLowPos                = 1000;
+    int sliderMedPos                = 1500;
+    int sliderHighPos               = 2000;
+    int sliderMaxPos                = 3000;
 }
