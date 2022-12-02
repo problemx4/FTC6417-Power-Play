ad648efdf0b4997c15366999fec6d3f49caf972f
Andrew Smith <AndrewTabSmith@gmail.com>
2022-12-02 13:20:27 -0500

    changed control structure and finalized safety measures

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BluCru6417/opmode/HolonomicTeleOp6417.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BluCru6417/opmode/HolonomicTeleOp6417.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode.BluCru6417.opmode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
@@ -20,31 +19,31 @@ import org.firstinspires.ftc.teamcode.BluCru6417.Hardware6417;
             a (x)                   - close grabber
             b (circle)              - open grabber
             dpad down               - turn on autoWrist
-            dpad left               - manual wrist back
-            dpad                    - manual wrist forward
+            dpad left               - manual wrist forward
+            dpad right              - manual wrist back
 
             gamepad 2:
             vertical left stick     - manual move arm
             vertical right stick    - manual move slider
-            left bumper             - reset arm encoder
+            left bumper             - toggle grabber
             right bumper            - reset slider encoder
-            left trigger            - retract wrist
-            right trigger           - bring slider down, no limiter
+            right trigger           - turn off limiter
             a (x)                   - ground slider position
             b (circle)              - low slider position
             x (square)              - medium slider position
             y (triangle)            - high slider position
-            dpad down               - down arm position
-            dpad right              - full forward arm position
+            dpad down               - manual wrist forward
+            dpad right              - retract wrist
             dpad left               - full backward arm position
-            dpad up                 - useful grabbing arm position
+            dpad up                 - manual wrist back
             */
 
 
 @TeleOp(name = "Main TeleOp", group = "TeleOp")
 public class HolonomicTeleOp6417 extends LinearOpMode implements ControlConstants{
 
-    ElapsedTime runtime = new ElapsedTime();
+    ElapsedTime armTimer = new ElapsedTime();
+    ElapsedTime slideTimer = new ElapsedTime();
 
     //Enums for state machine
     enum WRISTSTATE{
@@ -53,9 +52,17 @@ public class HolonomicTeleOp6417 extends LinearOpMode implements ControlConstant
         retractWrist
     }
     enum SLIDESTATE{
-        autoSlide,
+        targetBase,
+        targetLow,
+        targetMed,
+        targetHigh,
         manualSlide,
-        returnSlide
+    }
+
+    enum ARMSTATE{
+        manualArm,
+        targetDown,
+        targetBack,
     }
 
 
@@ -64,7 +71,6 @@ public class HolonomicTeleOp6417 extends LinearOpMode implements ControlConstant
         //initialize robot
         Hardware6417 robot = new Hardware6417(hardwareMap);
         telemetry.addData("Status", "Initialized");
-
 
         // make sure the imu gyro is calibrated before continuing.
         telemetry.addData("Mode", "calibrating...");
@@ -79,14 +85,13 @@ public class HolonomicTeleOp6417 extends LinearOpMode implements ControlConstant
         telemetry.addData("Mode", "calibrated!");
         telemetry.update();
 
-
         //setup robot
-        //robot.start();
-
+        robot.start();
 
         //wait for start and reset timer
         waitForStart();
-        runtime.reset();
+        armTimer.reset();
+        slideTimer.reset();
 
         //variables for controlling
         double verticalControl, horizontalControl, rotateControl, currentDrivePower;
@@ -95,14 +100,26 @@ public class HolonomicTeleOp6417 extends LinearOpMode implements ControlConstant
 
         boolean maintainHeading = true;
         boolean lastRB1 = false;
-        boolean lastLT2 = false;
+        boolean lastDR2 = false;
+        boolean lastLB2 = false;
+        boolean limiter = true;
+        boolean grabbing = true;
+        boolean lastSlideBusy = false;
 
-        SLIDESTATE slideState = SLIDESTATE.autoSlide;
-        WRISTSTATE wristState = WRISTSTATE.manualWrist;
+        SLIDESTATE slideState = SLIDESTATE.manualSlide;
+        SLIDESTATE lastSlideState = SLIDESTATE.manualSlide;
+        WRISTSTATE wristState = WRISTSTATE.autoWrist;
+        ARMSTATE armState = ARMSTATE.targetDown;
+        ARMSTATE lastArmState = ARMSTATE.targetDown;
 
 
         //Control loop
         while(opModeIsActive()){
+            //safety for switching controllers
+            if(gamepad2.start || gamepad1.start){
+                continue;
+            }
+
             //driving
             {
                 //get gamepad joystick and dpad variables for control
@@ -110,7 +127,7 @@ public class HolonomicTeleOp6417 extends LinearOpMode implements ControlConstant
                 horizontalControl = clipJoyInput(gamepad1.left_stick_x);
                 rotateControl = clipJoyInput(gamepad1.right_stick_x);
 
-                if(gamepad1.left_bumper){
+                if(gamepad1.left_bumper || slideState == SLIDESTATE.targetHigh || robot.leftSlider.getCurrentPosition() > sliderHighPos || robot.rightSlider.getCurrentPosition() > sliderHighPos){
                     currentDrivePower = maxSlowerDrivePower;
                 }
                 else if(gamepad1.left_trigger > triggerSens){
@@ -130,138 +147,166 @@ public class HolonomicTeleOp6417 extends LinearOpMode implements ControlConstant
                 lastRB1 = gamepad1.right_bumper;
             }
 
+            if(grabbing){
+                //slider control
+                {
+                    sliderControl = -1 * clipJoyInput(gamepad2.right_stick_y);
+                    limiter = !(gamepad2.right_trigger > triggerSens);
 
-            //slider control
-            {
-                sliderControl = -1 * clipJoyInput(gamepad2.right_stick_y);
+                    if(gamepad2.a){
+                        slideState = SLIDESTATE.targetBase;
+                    }
+                    if(gamepad2.b){
+                        slideState = SLIDESTATE.targetLow;
+                    }
+                    if(gamepad2.x){
+                        slideState = SLIDESTATE.targetMed;
+                    }
+                    if(gamepad2.y){
+                        slideState = SLIDESTATE.targetHigh;
+                    }
+                    if(Math.abs(sliderControl) >= sens){
+                        slideState = SLIDESTATE.manualSlide;
+                    }
 
-                switch(slideState){
-                    case autoSlide:
-                        //check each button and set target position
-                        if(gamepad2.a){
-                            robot.autoSlide(sliderBasePos);
-                        }
-                        if(gamepad2.b){
-                            robot.autoSlide(sliderLowPos);
-                        }
-                        if(gamepad2.x){
-                            robot.autoSlide(sliderMedPos);
-                        }
-                        if(gamepad2.y){
-                            robot.autoSlide(sliderHighPos);
-                        }
-                        //case to change state
-                        if(gamepad2.right_trigger > triggerSens){
-                            slideState = SLIDESTATE.returnSlide;
-                        }
-                        if(Math.abs(sliderControl) >= sens){
-                            slideState = SLIDESTATE.manualSlide;
-                        }
-                        break;
-                    case manualSlide:
-                        //control slider manually
-                        robot.manualSlide(maxSliderPower * sliderControl, true);
+                    if(slideState == SLIDESTATE.targetBase && lastSlideState != SLIDESTATE.targetBase){
+                        armState = ARMSTATE.targetDown;
+                        lastArmState = ARMSTATE.manualArm;
+                        robot.clearSliders(armClearDelta);
+                        slideTimer.reset();
+                    }
+                    lastSlideState = slideState;
 
-                        //case to change state (any button are pressed)
-                        if(gamepad2.right_trigger > triggerSens){
-                            slideState = SLIDESTATE.returnSlide;
+                    if(slideTimer.seconds() > sliderDelay){
+                        switch(slideState){
+                            case targetBase:
+                                robot.autoSlide(sliderBasePos);
+                                break;
+                            case targetLow:
+                                robot.autoSlide(sliderLowPos);
+                                break;
+                            case targetMed:
+                                robot.autoSlide(sliderMedPos);
+                                break;
+                            case targetHigh:
+                                robot.autoSlide(sliderHighPos);
+                                break;
+                            case manualSlide:
+                                //control slider manually
+                                robot.manualSlide(maxSliderPower * sliderControl, limiter);
+                                break;
                         }
-                        if(gamepad2.a || gamepad2.b || gamepad2.x || gamepad2.y){
-                            slideState = SLIDESTATE.autoSlide;
-                        }
-                        break;
-                    case returnSlide:
-                        //move slider down slowly to reset encoder
-                        robot.manualSlide(-returnSliderPower, false);
 
-                        //case to change state
-                        if(gamepad2.a || gamepad2.b || gamepad2.x || gamepad2.y){
-                            slideState = SLIDESTATE.autoSlide;
-                        }
-                        if(Math.abs(sliderControl) >= sens){
-                            slideState = SLIDESTATE.manualSlide;
-                        }
-                        break;
-                }
-            }
-
-
-            //arm control
-            {
-                armControl = -1 * clipJoyInput(gamepad2.left_stick_y);
-
-                //check each button and set target position
-                //servo arm code
-                if(gamepad2.dpad_down){
-                    robot.autoArm(armServoDownPos);
-                }
-                if(gamepad2.dpad_right){
-                    robot.autoArm(armServoForwardPos);
-                }
-                if(gamepad2.dpad_left){
-                    robot.autoArm(armServoBackwardsPos);
-                }
-                if(gamepad2.dpad_up){
-                    robot.autoArm(armServoUsefulPos);
-                }
-
-                robot.manualArm(manualWristDelta * armControl);
-            }
-
-
-            //wrist control
-            {
-                switch(wristState){
-                    case autoWrist:
-                        robot.autoWrist();
-                        //case to switch state
-                        if(gamepad2.left_trigger > triggerSens){
-                            wristState = WRISTSTATE.retractWrist;
-                        }
-                        else if(gamepad1.dpad_right || gamepad1.dpad_left){
-                            wristState = WRISTSTATE.manualWrist;
-                        }
-                        break;
-                    case manualWrist:
-                        //move forward
-                        if(gamepad1.dpad_right){
-                            robot.manualWrist(-manualWristDelta);
-                        }
-                        //move back
-                        if(gamepad1.dpad_left){
-                            robot.manualWrist(manualWristDelta);
-                        }
-                        //cases to swtich state
-                        if(gamepad2.left_trigger > triggerSens){
-                            wristState = WRISTSTATE.retractWrist;
-                        }
-                        if(gamepad1.dpad_down){
+                        if((!robot.leftSlider.isBusy() || !robot.rightSlider.isBusy()) && lastSlideBusy && (wristState == WRISTSTATE.retractWrist) && slideTimer.seconds() > sliderRetractDelay){
                             wristState = WRISTSTATE.autoWrist;
                         }
-                        break;
-                    case retractWrist:
-                        //retract wrist (clutch)
-                        if(gamepad2.left_trigger > triggerSens){
+                        lastSlideBusy = robot.leftSlider.isBusy() || robot.rightSlider.isBusy();
+                    }
+                }
+
+
+                //arm control
+                {
+                    armControl = -1 * clipJoyInput(gamepad2.left_stick_y);
+
+                    //check each button and set target position
+                    //servo arm code
+                    if(gamepad2.dpad_left){
+                        armState = ARMSTATE.targetBack;
+                    }
+                    if(Math.abs(armControl) > sens){
+                        armState = ARMSTATE.manualArm;
+                        lastArmState = ARMSTATE.manualArm;
+                    }
+
+                    if(armState != lastArmState){
+                        wristState = WRISTSTATE.retractWrist;
+                        armTimer.reset();
+                    }
+                    lastArmState = armState;
+
+                    if(armTimer.seconds() > armDelay){
+                        switch(armState){
+                            case targetDown:
+                                robot.autoArm(ControlConstants.armServoDownPos);
+                                break;
+                            case targetBack:
+                                robot.autoArm(ControlConstants.armServoBackwardsPos);
+                                break;
+                            case manualArm:
+                                robot.manualArm(manualServoDelta * armControl);
+                                break;
+                        }
+                    }
+                }
+
+
+                //wrist control
+                {
+                    switch(wristState){
+                        case autoWrist:
+                            robot.autoWrist();
+                            //case to switch state
+                            if(gamepad2.dpad_right){
+                                wristState = WRISTSTATE.retractWrist;
+                            }
+                            else if(gamepad1.dpad_right || gamepad1.dpad_left || gamepad2.dpad_down || gamepad2.dpad_up){
+                                wristState = WRISTSTATE.manualWrist;
+                            }
+                            break;
+                        case manualWrist:
+                            //move forward
+                            if(gamepad1.dpad_right || gamepad2.dpad_down){
+                                robot.manualWrist(-manualServoDelta);
+                            }
+                            //move back
+                            else if(gamepad1.dpad_left || gamepad2.dpad_up){
+                                robot.manualWrist(manualServoDelta);
+                            }
+                            //cases to swtich state
+                            if(gamepad2.dpad_right){
+                                wristState = WRISTSTATE.retractWrist;
+                            }
+                            if(gamepad1.dpad_down){
+                                wristState = WRISTSTATE.autoWrist;
+                            }
+                            break;
+                        case retractWrist:
+                            //retract wrist (clutch)
                             robot.retractWrist();
-                        }
-                        //case to switch state
-                        if(gamepad2.left_trigger < triggerSens && !lastLT2){
-                            wristState = WRISTSTATE.autoWrist;
-                        }
-                        break;
+                            //case to switch state
+                            if((!gamepad2.dpad_right && lastDR2) || gamepad1.dpad_down){
+                                wristState = WRISTSTATE.autoWrist;
+                            }
+                            lastDR2 = gamepad2.dpad_right;
+                            break;
+                    }
                 }
-
-                lastLT2 = gamepad2.left_trigger > triggerSens;
             }
-
 
             //grabber control
             {
-                if(gamepad1.a){
-                    robot.closeGrabber();
-                }
-                if(gamepad1.b){
-                    robot.openGrabber();
+                if(armTimer.seconds() > armFinishDelay && slideTimer.seconds() > sliderFinishDelay && ((!robot.leftSlider.isBusy() || !robot.rightSlider.isBusy()) || slideState == SLIDESTATE.manualSlide)){
+                    if(gamepad1.a){
+                        grabbing = true;
+                    }
+                    if(gamepad1.b){
+                        grabbing = false;
+                    }
+                    if(gamepad2.left_bumper && !lastLB2){
+                        grabbing = !grabbing;
+                    }
+                    lastLB2 = gamepad2.left_bumper;
+
+                    if(grabbing){
+                        robot.closeGrabber();
+                    }
+                    else{
+                        robot.openGrabber();
+                        if(slideState == SLIDESTATE.manualSlide){
+                            robot.manualSlide(0,true);
+                        }
+                    }
                 }
             }
 
@@ -282,12 +327,16 @@ public class HolonomicTeleOp6417 extends LinearOpMode implements ControlConstant
             //telemetry
             {
                 telemetry.addData("maintain heading", maintainHeading);
+                telemetry.addData("ArmState", armState);
+                telemetry.addData("WristState", wristState);
+                telemetry.addData("SliderState", slideState);
+                telemetry.addData("Grabbing?", grabbing);
 
                 robot.telemetry(telemetry);
             }
         }
         //stop robot after end
-        robot.end();
+        //robot.end();
     }
 
 
